8bdfddb65a9754487becca63476e21d602554794
albertchan <albert.shun.dat.chan@gmail>
2016-06-27 19:34:41 -0400

    Generated copyright/license header using Eclipse Copyright Tool

--- src/main/java/chan/shundat/albert/sqlbuilder/LiteralBoolean.java
+++ src/main/java/chan/shundat/albert/sqlbuilder/LiteralBoolean.java
@@ -1,3 +1,10 @@
+/*******************************************************************************
+ * Copyright (c) 2016 Albert Shun-Dat Chan
+ *
+ * Licensed under MIT License
+ * See LICENSE file at the root of the project or
+ * https://github.com/Javalbert/sql-builder-orm/blob/master/LICENSE
+ *******************************************************************************/
 package chan.shundat.albert.sqlbuilder;
 
 public class LiteralBoolean extends Literal<Boolean> {
